30e49299ffc2c32e326bf90698107a6090e3117b
Nick Law <nick.craig.law@gmail.com>
2021-01-07 15:47:32 +0000

    Updated nwipe to 0.30

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.29.006
+NWIPE_VERSION = v0.30.001
 NWIPE_SITE = $(call github,PartialVolume,nwipe,$(NWIPE_VERSION))
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
 define NWIPE_INITSH
-	(cd $(@D) && ./init.sh);
+	(cd $(@D) && ./autogen.sh);
 endef
 
 NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
