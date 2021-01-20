eba82c6ef64c61d4b0727e7a7f96f4bca980f903
Nick Law <nick.craig.law@gmail.com>
2021-01-20 09:39:21 +0000

    Add the ShredOS icon, autorun.inf and a warning in README.txt specifically for when a ShredOS USB drive is plugged into a running Windows system. The ShredOS icon will appear and the message ShredOS - Danger. As a warning for users that may accidently come across this USB flash drive and wonder what's on it.

---
+++ board/shredos/README.txt
@@ -0,0 +1,9 @@
+ShredOS is a USB bootable (BIOS or UEFI) small linux distribution with the sole purpose of securely erasing the entire contents of your disks using the program nwipe
+
+https://github.com/PartialVolume/shredos.2020.02
+https://github.com/PartialVolume/shredos.i686
+https://github.com/PartialVolume/shredos.x86_64
+
+!!! THIS MEDIUM CAN IRREVERSIBLY AND SECURELY DESTROY YOUR HARD DRIVE DATA !!!
+
+!!!! USE IF YOU KNOW WHAT YOU ARE DOING AND AT YOUR OWN RISK !!!!
--- board/shredos/doimg.sh
+++ board/shredos/doimg.sh
@@ -3,6 +3,13 @@
 cp "board/shredos/grub.cfg"                "${BINARIES_DIR}/grub.cfg"    || exit 1
 cp "board/shredos/bootx64.efi"             "${BINARIES_DIR}/bootx64.efi" || exit 1
 cp "${HOST_DIR}/lib/grub/i386-pc/boot.img" "${BINARIES_DIR}/boot.img"    || exit 1
+
+# copy the ShredOS icon and windows files, if a stick is plugged into a  Windows system
+# it is identified as ShredOS - Dangerous as a warning to users that that know what ShredOS is.
+cp "board/shredos/autorun.inf"             "${BINARIES_DIR}/autorun.inf" || exit 1
+cp "board/shredos/README.txt"              "${BINARIES_DIR}/README.txt"  || exit 1
+cp "board/shredos/shredos.ico"             "${BINARIES_DIR}/shredos.ico" || exit 1
+
 rm -rf "${BUILD_DIR}/genimage.tmp"                                       || exit 1
 genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}" --outputpath="${BINARIES_DIR}" --config="board/shredos/genimage.cfg" --tmppath="${BUILD_DIR}/genimage.tmp" || exit 1
 
