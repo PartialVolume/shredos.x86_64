e409d4a825350ed1dc74de3bfc9326fb510ad399
PartialVolume <nick.craig.law@gmail.com>
2021-01-24 19:12:48 +0000

    1. Added vendor specific USB host controller drivers uhci_hcd. 2. The banner and final .img file name now obtain their version information from ../board/shredos/version.txt. This file contains the version number in the form 2020.05.008_x86-64_0.30.001, where 2020.05 is the buildroot version, 008 is the Shredos minor version, x86-64 is the architecture, 0.30.001 is the nwipe version.

--- board/shredos/doimg.sh
+++ board/shredos/doimg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+version=`cat board/shredos/version.txt`
+
 cp "board/shredos/grub.cfg"                "${BINARIES_DIR}/grub.cfg"    || exit 1
 cp "board/shredos/bootx64.efi"             "${BINARIES_DIR}/bootx64.efi" || exit 1
 cp "${HOST_DIR}/lib/grub/i386-pc/boot.img" "${BINARIES_DIR}/boot.img"    || exit 1
@@ -14,8 +16,7 @@ rm -rf "${BUILD_DIR}/genimage.tmp"                                       || exit
 genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}" --outputpath="${BINARIES_DIR}" --config="board/shredos/genimage.cfg" --tmppath="${BUILD_DIR}/genimage.tmp" || exit 1
 
 # renaming
-SUFFIXIMG=2020.05.007_x86_64-0.30.001_$(date +%Y%m%d)
-#SUFFIXIMG=$(date +%Y%m%d)
+SUFFIXIMG="${version}_$(date +%Y%m%d)"
 FINAL_IMAGE_PATH="${BINARIES_DIR}/shredos-${SUFFIXIMG}.img"
 mv "${BINARIES_DIR}/shredos.img" "${FINAL_IMAGE_PATH}" || exit 1
 
