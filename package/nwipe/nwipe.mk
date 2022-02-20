e9be64e3ab0fc9bdc604151046b6c9d701239c96
PartialVolume <nick.craig.law@gmail.com>
2022-02-20 14:52:42 +0000

    Add DRM drivers for AMDGPU, Radeon, Nouveau, I915, GMA500, UDL for improved functioning of suspend and re-wake from RAM required to unfreeze drives ready for upcoming secure erase implementation in nwipe. Added LCD devices L4F00242T03, LMS283GF05. Added Backlight KTD253, GPIO. Added boot logo support. Removed buildroot manual packages.

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.32.014
+NWIPE_VERSION = v0.32.023
 NWIPE_SITE = $(call github,partialvolume,nwipe,$(NWIPE_VERSION))
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
