c28daaea962e95c05adad0e5af8f509262548b39
Fab <fabian@thobe.eu>
2024-08-27 08:16:41 +0800

    Fixed hash file and source origin site

--- package/parallel/parallel.mk
+++ package/parallel/parallel.mk
@@ -9,7 +9,7 @@
 
 PARALLEL_VERSION = 20240822
 PARALLEL_SOURCE = parallel-$(PARALLEL_VERSION).tar.bz2
-PARALLEL_SITE = https://www.gnu.org/software/parallel
+PARALLEL_SITE = https://ftp.gnu.org/gnu/parallel
 PARALLEL_LICENSE = GPL-3.0-only
 PARALLEL_LICENSE_FILES = LICENSES/GPL-3.0-only.txt
 
