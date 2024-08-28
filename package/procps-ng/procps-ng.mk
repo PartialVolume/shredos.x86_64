0c7e260bf48b206ab8d2f5e7cb3dd03c31aa2393
Fab <fabian@thobe.eu>
2024-08-28 11:24:27 +0800

    Bugfixed procps-ng dependency
    Fixed typo in config.in that caused procps-ng to appear twice effectivly resolving
    #274 #277 the hopefully last time

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -1,26 +1,26 @@
 ################################################################################
 #
-# procps-NG
+# procps-ng
 #
 ################################################################################
 
-PROCPS-NG_VERSION = 3.3.17
-PROCPS-NG_SOURCE = procps-NG-$(PROCPS-NG_VERSION).tar.xz
-PROCPS-NG_SITE = http://downloads.sourceforge.net/project/procps-NG/Production
-PROCPS-NG_LICENSE = GPL-2.0+, LGPL-2.0+ (libproc and libps)
-PROCPS-NG_LICENSE_FILES = COPYING COPYING.LIB
-PROCPS-NG_CPE_ID_VALID = YES
-PROCPS-NG_INSTALL_STAGING = YES
+PROCPS_NG_VERSION = 3.3.17
+PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
+PROCPS_NG_SITE = http://downloads.sourceforge.net/project/procps-ng/Production
+PROCPS_NG_LICENSE = GPL-2.0+, LGPL-2.0+ (libproc and libps)
+PROCPS_NG_LICENSE_FILES = COPYING COPYING.LIB
+PROCPS_NG_CPE_ID_VALID = YES
+PROCPS_NG_INSTALL_STAGING = YES
 # We're patching configure.ac
-PROCPS-NG_AUTORECONF = YES
-PROCPS-NG_DEPENDENCIES = ncurses host-pkgconf $(TARGET_NLS_DEPENDENCIES)
-PROCPS-NG_CONF_OPTS = LIBS=$(TARGET_NLS_LIBS)
+PROCPS_NG_AUTORECONF = YES
+PROCPS_NG_DEPENDENCIES = ncurses host-pkgconf $(TARGET_NLS_DEPENDENCIES)
+PROCPS_NG_CONF_OPTS = LIBS=$(TARGET_NLS_LIBS)
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
-PROCPS-NG_DEPENDENCIES += systemd
-PROCPS-NG_CONF_OPTS += --with-systemd
+PROCPS_NG_DEPENDENCIES += systemd
+PROCPS_NG_CONF_OPTS += --with-systemd
 else
-PROCPS-NG_CONF_OPTS += --without-systemd
+PROCPS_NG_CONF_OPTS += --without-systemd
 endif
 
 # Make sure binaries get installed in /bin, as busybox does, so that we
@@ -28,39 +28,39 @@ endif
 # Make sure libprocps.pc is installed in STAGING_DIR/usr/lib/pkgconfig/
 # otherwise it's installed in STAGING_DIR/lib/pkgconfig/ breaking
 # pkg-config --libs libprocps.
-PROCPS-NG_CONF_OPTS += --exec-prefix=/ \
+PROCPS_NG_CONF_OPTS += --exec-prefix=/ \
 	--libdir=/usr/lib
 
 # Allows unicode characters to show in 'watch'
 ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
-PROCPS-NG_CONF_OPTS += \
+PROCPS_NG_CONF_OPTS += \
 	--enable-watch8bit
 endif
 
 ifeq ($(BR2_USE_WCHAR),)
-PROCPS-NG_CONF_OPTS += CPPFLAGS=-DOFF_XTRAWIDE
+PROCPS_NG_CONF_OPTS += CPPFLAGS=-DOFF_XTRAWIDE
 endif
 
 # numa support requires libdl, so explicitly disable it when
 # BR2_STATIC_LIBS=y
 ifeq ($(BR2_STATIC_LIBS),y)
-PROCPS-NG_CONF_OPTS += --disable-numa
+PROCPS_NG_CONF_OPTS += --disable-numa
 endif
 
 # w requires utmp.h
 ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
-PROCPS-NG_CONF_OPTS += --disable-w
+PROCPS_NG_CONF_OPTS += --disable-w
 else
-PROCPS-NG_CONF_OPTS += --enable-w
+PROCPS_NG_CONF_OPTS += --enable-w
 endif
 
 # Avoid installing S02sysctl, since openrc provides /etc/init.d/sysctl.
-define PROCPS-NG_INSTALL_INIT_OPENRC
+define PROCPS_NG_INSTALL_INIT_OPENRC
 	@:
 endef
 
-define PROCPS-NG_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 755 package/procps-NG/S02sysctl \
+define PROCPS_NG_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/procps-ng/S02sysctl \
 		$(TARGET_DIR)/etc/init.d/S02sysctl
 endef
 
