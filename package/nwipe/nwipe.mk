bbfb8cd82e679928a570635076c6b5d8e8444afa
Fabian Druschke <fdruschke@outlook.com>
2024-09-09 18:21:33 +0200

    Replaced the current nwipe.mk download option by a git submodule, pointing to master branch.

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,15 +4,29 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.37
-NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
+# Clones the nwipe master branch to external/nwipe auf der master branch
+NWIPE_VERSION = master
+NWIPE_SITE = NO_SITE
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
+# The path for git init
+NWIPE_SUBMODULE_PATH = $(TOPDIR)/external/nwipe
+
 define NWIPE_INITSH
-	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
+	(cd $(NWIPE_SUBMODULE_PATH) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(NWIPE_SUBMODULE_PATH) && ./002-nwipe-banner-patch.sh && PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
 NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
 
+# Disables download procedure
+define DOWNLOAD_NWIPE
+	true
+endef
+
+# Copies the source out of the source path
+define NWIPE_EXTRACT_CMDS
+	rsync -a $(NWIPE_SUBMODULE_PATH)/ $(@D)/
+endef
 
 $(eval $(autotools-package))
+
