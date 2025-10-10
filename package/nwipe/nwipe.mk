92c8ed56f9cb3fc29f07992ce1ebaa671abc3035
Daan Selen <dselen@systemec.nl>
2025-10-10 16:23:44 +0200

    chore: bump nwipe

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.38
+NWIPE_VERSION = v0.39
 NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
