41da80df70ec3f2f5e49af4a8c02809612173c4d
DaanSelen <dselen@systemec.nl>
2025-10-16 13:31:26 +0200

    chore: clean the .mk file of nwipe

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,15 +4,18 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.39
+NWIPE_VERSION = 0.39
 NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
 define NWIPE_INITSH
-	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
+	cd $(NWIPE_SRCDIR) && \
+	cp $(NWIPE_PKGDIR)/002-nwipe-banner-patch.sh . && \
+	./002-nwipe-banner-patch.sh && \
+	PATH="$(HOST_DIR)/bin:$$PATH" ./autogen.sh
 endef
 
-NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
-
+#NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
+NWIPE_PRE_CONFIGURE_HOOKS += NWIPE_INITSH
 
 $(eval $(autotools-package))
