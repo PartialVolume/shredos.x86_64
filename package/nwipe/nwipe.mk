3460f24384130b0ddb020c5e048b64121e03727e
Daan Selen <dselen@nerthus.nl>
2025-10-17 02:11:44 -0500

    chore: update .mk and .hash files

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -5,14 +5,13 @@
 ################################################################################
 
 NWIPE_VERSION = 0.39
-NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
+NWIPE_SOURCE = v$(NWIPE_VERSION).tar.gz
+NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe/archive/refs/tags
+
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
 define NWIPE_INITSH
-	cd $(NWIPE_SRCDIR) && \
-	cp $(NWIPE_PKGDIR)/002-nwipe-banner-patch.sh . && \
-	./002-nwipe-banner-patch.sh && \
-	PATH="$(HOST_DIR)/bin:$$PATH" ./autogen.sh
+    (cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
 #NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
