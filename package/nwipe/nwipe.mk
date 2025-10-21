6a4ad199ea64bb39dd44a014ddc369dd323d13ad
Daan Selen <dselen@nerthus.nl>
2025-10-21 03:39:56 -0500

    chore: try to integrate https://github.com/PartialVolume/shredos.x86_64/pull/322

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,17 +4,23 @@
 #
 ################################################################################
 
-NWIPE_VERSION = 0.39
-NWIPE_SOURCE = v$(NWIPE_VERSION).tar.gz
-NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe/archive/refs/tags
+NWIPE_VERSION = ed74492
 
+# Specify the Git source:
+#  - NWIPE_SITE points to the repository
+#  - NWIPE_SITE_METHOD = git ensures that Buildroot performs a Git checkout
+NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe.git
+NWIPE_SITE_METHOD = git
+
+# Dependencies remain the same (adjust them if you need additional ones)
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
 define NWIPE_INITSH
-    (cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
+        (cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) \
+                && ./002-nwipe-banner-patch.sh \
+                && PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
-#NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
 NWIPE_PRE_CONFIGURE_HOOKS += NWIPE_INITSH
 
 $(eval $(autotools-package))
