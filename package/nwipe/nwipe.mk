e1414a8f43755b59409ddb4b3af4b0265dfc7b7d
DaanSelen <80752476+DaanSelen@users.noreply.github.com>
2025-10-22 22:48:10 +0200

    chore(pkg): update nwipe version to 0.39 (#380)
    * chore: bump nwipe
    
    * chore: clean the .mk file of nwipe
    
    * chore: update .mk and .hash files
    
    * chore: add tar.gz into gitignore
    
    * chore: try to integrate https://github.com/PartialVolume/shredos.x86_64/pull/322
    
    * Revert "chore: try to integrate https://github.com/PartialVolume/shredos.x86_64/pull/322"
    
    This reverts commit 6a4ad199ea64bb39dd44a014ddc369dd323d13ad.
    
    * chore: bump image name
    
    * Revert "chore: bump image name"
    
    This reverts commit ec70d375b5be56706be81bb024d69ce1087d95e0.
    
    ---------
    
    Co-authored-by: Daan Selen <dselen@systemec.nl>

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,15 +4,17 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.38
-NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
+NWIPE_VERSION = 0.39
+NWIPE_SOURCE = v$(NWIPE_VERSION).tar.gz
+NWIPE_SITE = https://github.com/martijnvanbrummelen/nwipe/archive/refs/tags
+
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
 
 define NWIPE_INITSH
-	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
+    (cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
 endef
 
-NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
-
+#NWIPE_POST_PATCH_HOOKS += NWIPE_INITSH
+NWIPE_PRE_CONFIGURE_HOOKS += NWIPE_INITSH
 
 $(eval $(autotools-package))
