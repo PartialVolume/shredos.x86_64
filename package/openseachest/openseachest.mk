82c1387ab0ea40b1a5321ce015d77207319cb5aa
PartialVolume <22084881+PartialVolume@users.noreply.github.com>
2026-01-09 00:23:52 +0000

    Upgraded Seagate's openSeaChest tools to 25.05.3 and restored busybox.config back to it's default location

--- package/openseachest/openseachest.mk
+++ package/openseachest/openseachest.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-OPENSEACHEST_VERSION = 24.08.1
+OPENSEACHEST_VERSION = 25.05.3
 OPENSEACHEST_SOURCE = SourceCode_With_Submodules.tar.xz
-OPENSEACHEST_SITE = https://github.com/Seagate/openSeaChest/releases/download/v24.08.1
+OPENSEACHEST_SITE = https://github.com/Seagate/openSeaChest/releases/download/v25.05.3
 OPENSEACHEST_LICENSE = MPL-2.0
 OPENSEACHEST_LICENSE_FILES = COPYING
 OPENSEACHEST_INSTALL_STAGING = YES
