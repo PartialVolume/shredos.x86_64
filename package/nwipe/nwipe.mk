7c48bb4f9de097030f0a73bfd029518821ecede0
PartialVolume <22084881+PartialVolume@users.noreply.github.com>
2026-01-17 23:25:59 +0000

    Prior to building nwipe, determine architecture and update the nwipe banner as appropriate, either i686 or x86-64. Prior to this patch the banner was hardcoded and had to be manually edited in board/shredos/fsoverlay/etc/shredos/version.txt. When switching between architectures the banner will be automatically updated and manual editing of architecture is no longer necessary.

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -24,11 +24,28 @@ NWIPE_SITE_METHOD = git
 
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils libconfig
 
+################################################################################
+# Version architecture nwipe banner updater (pre-build)
+################################################################################
+
+SHREDOS_VERSION_FILE = board/shredos/fsoverlay/etc/shredos/version.txt
+
 define NWIPE_INITSH
+	@echo "Updating version.txt for architecture: $(BR2_ARCH)"
+
+	@if [ "$(BR2_ARCH)" = "i686" ]; then \
+		sed -i 's/\(^.*_\)\(x86-64\|i686\)\(_.*\)/\1i686\3/' $(SHREDOS_VERSION_FILE); \
+	elif [ "$(BR2_ARCH)" = "x86_64" ]; then \
+		sed -i 's/\(^.*_\)\(x86-64\|i686\)\(_.*\)/\1x86-64\3/' $(SHREDOS_VERSION_FILE); \
+	else \
+		echo "Unsupported architecture: $(BR2_ARCH)"; \
+		exit 1; \
+	fi
+
 	(cd $(@D) && \
-		cp ../../../package/nwipe/002-nwipe-banner-patch.sh . && \
-		./002-nwipe-banner-patch.sh && \
-		PATH="../../host/bin:${PATH}" ./autogen.sh)
+	cp ../../../package/nwipe/002-nwipe-banner-patch.sh . && \
+	./002-nwipe-banner-patch.sh && \
+	PATH="../../host/bin:${PATH}" ./autogen.sh)
 endef
 
 # Pre-configure hook, as a post-patch hook would not get triggered on a package
